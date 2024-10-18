bb7036ae7acd22cb58f2a94b81a7c8f3b53275d0
frakoDev <frako23@gmail.com>
2024-10-18 16:00:26 -0400

    ajustes esteticos

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,15 +1,11 @@
 interface Messages {
-    agenda: string
     guia: string
     descarga: string
-    consulta: string
     asesorar: string
 }
 
 export const messages: Messages = {
-    agenda: "Agenda una Consulta",
     guia: "Quiero mi Guía Gratis",
     descarga: "Quiero Descargar Finami",
-    consulta: "Quiero mi Consulta Gratuita",
-    asesorar: "Quiero Asesorarme",
+    asesorar: "Quiero mi Asesoría Gratuita",
 }
